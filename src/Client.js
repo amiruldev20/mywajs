9460bdf4c1f2649e1af7125d494a7b40d0479ec8
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-04-12 11:07:18 +0700

    fixed msg media

--- src/Client.js
+++ src/Client.js
@@ -18,7 +18,7 @@ const { WhatsWebURL, DefaultOptions, Events, WAState } = require('./util/Constan
 const { ExposeStore, LoadUtils } = require('./util/Injected');
 const ChatFactory = require('./factories/ChatFactory');
 const ContactFactory = require('./factories/ContactFactory');
-const { ClientInfo, Message, Chat } = require('./func');
+const { ClientInfo, Message, Chat, MessageMedia } = require('./func');
 const LegacySessionAuth = require('./auth/LegacySessionAuth');
 const NoAuth = require('./auth/NoAuth');
 
