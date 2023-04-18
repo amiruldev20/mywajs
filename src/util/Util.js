6d39bcd52c45124892710eecc0ee3d6f2c73ff24
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-04-19 04:45:10 +0700

    add formatted size

--- src/util/Util.js
+++ src/util/Util.js
@@ -264,6 +264,17 @@ class Util {
         }
     }
 
+    static formatSize(bytes) {
+        if (bytes >= 1000000000) { bytes = (bytes / 1000000000).toFixed(2) + " GB"; }
+        else if (bytes >= 1000000) { bytes = (bytes / 1000000).toFixed(2) + " MB"; }
+        else if (bytes >= 1000) { bytes = (bytes / 1000).toFixed(2) + " KB"; }
+        else if (bytes > 1) { bytes = bytes + " bytes"; }
+        else if (bytes == 1) { bytes = bytes + " byte"; }
+        else { bytes = "0 bytes"; }
+        return bytes;
+        }
+
+
 }
 
 module.exports = Util;
