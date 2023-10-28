e3e0fc3b331c04410738e38140cfa9d9807f4d37
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-10-29 05:28:26 +0700

    update whatsapp version

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -7,7 +7,7 @@ exports.DefaultOptions = {
         headless: true,
         defaultViewport: null
     },
-    webVersion: '2.2346.52',
+    webVersion: '2.2347.56',
     webVersionCache: {
         type: 'local',
     },
